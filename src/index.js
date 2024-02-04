cc90d622cd6647dfdfac3c320e2b334be24d763b
bachurskii <georgijbacurskij@gmail.com>
2024-02-04 15:26:32 +0200

    update

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Provider } from "react-redux";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/Country-app/build/index.html">
+      <BrowserRouter basename="/Country-app">
         <App />
       </BrowserRouter>
     </Provider>
